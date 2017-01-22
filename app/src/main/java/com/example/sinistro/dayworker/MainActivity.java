eca1b68b06967574ab9ebb6838d2ae3b2c90f319
Noe Lomeli <noelomeli@student.hartnell.edu>
2017-01-22 10:58:18 -0800

    Change action bar color

--- app/src/main/java/com/example/sinistro/dayworker/MainActivity.java
+++ app/src/main/java/com/example/sinistro/dayworker/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         final Button login = (Button)findViewById(R.id.findWork);
 
