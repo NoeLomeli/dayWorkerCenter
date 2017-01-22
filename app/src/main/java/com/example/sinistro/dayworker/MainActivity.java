9276f44b502399388518e83b22eea93195ced2f4
Noe Lomeli <noelomeli@student.hartnell.edu>
2017-01-21 22:59:25 -0800

    Create main menu buttons

---
+++ app/src/main/java/com/example/sinistro/dayworker/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.sinistro.dayworker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button login = (Button)findViewById(R.id.findWork);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent findInt = new Intent(MainActivity.this, UserRegisterActivity.class);
+                MainActivity.this.startActivity(findInt);
+            }
+        });
+
+    }
+
+
+//    @Override
+//    public void onClick(View v) {
+//        switch(v.getId()){
+//            case R.id.findWork:
+//                Intent registrarInt = new Intent(MainActivity.this, UserRegisterActivity.class);
+//                MainActivity.this.startActivity(registrarInt);
+//    }
+}
